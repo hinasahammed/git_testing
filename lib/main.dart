5db0cecbaaee8086af66d1d7317f00317fa1a94d
Ahammed Hinas <82311707+hinasahammed@users.noreply.github.com>
2026-01-21 16:07:06 +0530

    Apply official Dart formatting

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const MyHomePage(title: 'Flutter Demo Page'),
     );
   }
